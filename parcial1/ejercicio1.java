32a93a86ad00e963eaf15439b756abb7caa29df2
Juan Obando <juanobando.developer@gmail.com>
2023-05-01 07:49:52 -0500

    refactor: change array nums by arrayList, remove count variable and change while structure by do-while structure

--- parcial1/ejercicio1.java
+++ parcial1/ejercicio1.java
@@ -4,34 +4,31 @@ package parcial1;
  * 1. Ejercicio que pida 100 numeros y que al ingresar el numero 99 el programa termine, al terminar debe mostrar los numeros pares ingresados.
  */
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 class ejercicio1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int[] nums = new int[100];
+        // create an array of integers (mutable array)
+        List<Integer> nums = new ArrayList<Integer>();
         int num = 0;
-        int count = 0;
-        // 99 is the exit number
-        while (num != 99) {
-            // if the array is full, exit the loop
-            if (count == nums.length) {
-                break;
-            }
+        // while num is not 99 and the array is not full do the following
+        do {
+            // ask for a number
             System.out.println("Please type a number:");
+            // store the number in the variable num
             num = input.nextInt();
-            // if the number is even, add it to the array
+            // only if the number is even add it to the array
             if (num % 2 == 0) {
-                nums[count] = num;
-                count++;
+                nums.add(num);
             }
-        }
-        // remove the zeros from the array
-        nums = Arrays.stream(nums).filter(element -> element != 0).toArray();
-        for (int i = 0; i < nums.length; i++) {
-            // print the even numbers
-            System.out.println("even number: " + nums[i]);
+        } while (num != 99 && nums.size() < 100);
+        // print the even numbers in the array
+        for (int i = 0; i < nums.size(); i++) {
+            // show number by number with ArrayList.get() method
+            System.out.println("even number: " + nums.get(i));
         }
     }
 }
