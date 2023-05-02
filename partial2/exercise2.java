0aee9662b1560d324a196852e7e631d826f7c681
Juan Obando <juanobando.developer@gmail.com>
2023-05-01 19:35:20 -0500

    refactor: add fibonacci size like a constant, print table with format

--- partial2/exercise2.java
+++ partial2/exercise2.java
@@ -8,80 +8,81 @@ import java.util.Random;
 
 class exercise2 {
     public static void main(String args[]) {
-        // create instance of Random class
+        // create constants
+        final int FIBONACCI_SIZE = 100;
+        final String HEADER = "Position       Number          Binary\n------------------------------------";
+
+        // create variables and arrays
         Random aleatorio = new Random();
-        // create arrays
-        long[] fibonacci = new long[100];
-        String[] binary = new String[100];
-        // fill fibonacci array with -1
+        long[] fibonacci = new long[FIBONACCI_SIZE];
+        String[] binary = new String[FIBONACCI_SIZE];
+
+        // fill the fibonacci array with -1
         for (int i = 0; i < fibonacci.length; i++) {
             fibonacci[i] = -1;
         }
-        // generate 100 random numbers between 1 and 10000 and add them to the fibonacci
-        // array if they are fibonacci numbers
+
+        // fill the fibonacci array with random numbers
         for (int i = 0; i < fibonacci.length; i++) {
-            // generate random number
-            int randNumber = aleatorio.nextInt((10000 - 0) + 1) + 0;
-            // generate fibonacci sequence until the number is greater than the random
-            // number
+            // generate a random number between 0 and 10000
+            int randNumber = aleatorio.nextInt((10000 - 0 + 1) + 0);
+            // check if the number is a fibonacci number
             for (int j = 0; fibonacciSequence(j) <= randNumber; j++) {
-                // if the random number is equal to the fibonacci number, add it to the
-                // fibonacci array
+                // if the number is a fibonacci number, add it to the array
                 if (randNumber == fibonacciSequence(j)) {
                     fibonacci[i] = randNumber;
                 }
             }
-            // if the number is not a fibonacci number, go back one position in the array
-            // and try again
+            // if the number is not a fibonacci number, go back one position
             if (fibonacci[i] == -1) {
                 i--;
             }
         }
+        // print the table
+        System.out.println(HEADER);
+        // print the fibonacci numbers and their binary numbers
         for (int i = 0; i < fibonacci.length; i++) {
-            // print table header
-            System.out.println((i == 0) ? "Position       Number          Binary" : "");
-            System.out.println((i == 0) ? "------------------------------------" : "");
-            // get binary number from fibonacci number
+            // convert the fibonacci number to binary
             binary[i] = binaryNumber(fibonacci[i]);
-            // print table content (position, number, binary number)
-            System.out.println("/   " + (i + 1) + ".      /   " + fibonacci[i] + "        /   " + binary[i] + " /");
+            // print the fibonacci number, its position and its binary number
+            System.out.printf("    %d           %d            %s", i + 1, fibonacci[i], binary[i] + "\n");
         }
     }
 
     public static long fibonacciSequence(int number) {
-        // if number is less than or equal to 0, return 0
+        // if the number is less than or equal to 0, return 0
         if (number <= 0) {
             return 0;
-        } else if (number == 1) { // if number is equal to 1, return 1
+        } else if (number == 1) { // if the number is equal to 1, return 1
             return 1;
-        } else { // if number is greater than 1, return the sum of the previous two numbers
+        } else { // if the number is greater than 1, return the sum of the previous two numbers
             return fibonacciSequence(number - 1) + fibonacciSequence(number - 2);
         }
     }
 
     public static String binaryNumber(long num) {
-        // create variable to store binary number
+        // create a string to store the binary number
         String binary = "";
-        // while number is greater than 0
+        // while number is not 0
         while (num != 0) {
-            // add the remainder of the division between the number and 2 to the binary
+            // add the remainder of the division to the string
             binary += num % 2;
             // divide the number by 2
             num /= 2;
         }
-        // return the reversed binary number (because it is generated backwards)
+        // return the reversed string because the binary number is backwards
         return reverse(binary);
     }
 
     public static String reverse(String text) {
-        // create variable to store reversed string
+        // create a string to store the reversed string
         String reversed = "0";
-        // iterate over string backwards
+        // for each character in the string, add it to the reversed string
         for (int i = text.length() - 1; i >= 0; i--) {
-            // add each character to the reversed string
+            // add the last character of the string to the reversed string
             reversed += text.charAt(i);
         }
-        // return reversed string
+        // return the reversed string
         return reversed;
     }
 }
